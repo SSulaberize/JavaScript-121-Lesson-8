0af9c9023f2347e70334679b3215f8e84fb46c27
SSulaberidze <ssulberidze@gmail.com>
2022-10-18 23:11:50 +0400

    Second major update

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let collection = [
     {
         id: 1,
-        imageSource: 'images/image.1.jpg',
+        imageSource: 'images/image1.jpg',
         title: 'picture title 1',
     },
     {
         id: 2,
-        imageSource: 'images/image.2.jpg',
+        imageSource: 'images/image2.jpg',
         title: 'picture title 2',
     },
     {
         id: 3,
-        imageSource: 'images/image.3.jpg',
+        imageSource: 'images/image3.jpg',
         title: 'picture title 3',
     },
     {
         id: 4,
-        imageSource: 'images/image.4.jpg',
+        imageSource: 'images/image4.jpg',
         title: 'picture title 4',
     },
     {
         id: 5,
-        imageSource: 'images/image.5.jpg',
+        imageSource: 'images/image5.jpg',
         title: 'picture title 5',
     },
 ];
@@ -42,9 +42,9 @@ function createImgTag(item){
     let imgTag = document.createElement('img');
     imgTag.setAttribute('src', item.imageSource);
     imgTag.setAttribute('alt', item.title);
+    imgTag.classList.add('imgslide');
 
     return imgTag;
-    
 }
 
 function createH3Tag(item){
@@ -52,27 +52,59 @@ function createH3Tag(item){
    titleH3.innerText = item.title;
 
    return titleH3;
-    
+}
+
+function createDots(){
+    let dots = document.createElement('div');
+    dots.classList.add('dot-wrapper');
+
+    collection.forEach(element => {
+        let childDot = document.createElement('div');
+        childDot.classList.add('dot');
+        dots.appendChild(childDot);
+    })
+    return dots;
 }
 
 function setSlide(){
+    sliderContent.innerHTML = ' ';
     let slideItem = createDivTag(collection[indexSlider]);
     let tagImg = createImgTag(collection[indexSlider]); 
     let headerTag = createH3Tag(collection[indexSlider]);
+    let dots = createDots();
 
     slideItem.appendChild(tagImg);
     slideItem.appendChild(headerTag);
     sliderContent.appendChild(slideItem);
+    sliderContent.appendChild(dots);
 }
 
-leftArrow.addEventListener('click', function (){
-indexSlider -=1
-setSlide();
-});
-
-rightArrow.addEventListener('click', function (){
-    indexSlider +=1
+function leftArrowClick() {
+    if (indexSlider == 0) {
+        indexSlider = collection.length-1;
+        setSlide();
+        return;
+    }
+    indexSlider =-1;
     setSlide();
-    });
+}
+
+leftArrow.addEventListener('click', leftArrowClick);
+
+function rightArrowClick() {
+    if (indexSlider == collection.length - 1){
+        indexSlider = 0;
+        setSlide();
+        return;
+    }
+    indexSlider =+1;
+    setSlide();
+}
+
+rightArrow.addEventListener('click', rightArrowClick);
+
+setInterval(() => {
+    rightArrowClick();
+}, 2000);
 
 setSlide();
